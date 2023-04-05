e0f36194d78a5d98ecd025423901df317a44659c
andrewlunbong <andrewlunbong@gmail.com>
2023-04-05 17:13:51 +0100

    homework

---
+++ src/python_functions_practice.py
@@ -0,0 +1,72 @@
+def return_10():
+    return 10
+
+def add(first_number, second_number):
+    sum_of_numbers = first_number + second_number
+    return sum_of_numbers
+
+def subtract(first_number, second_number):
+    sum_of_numbers = first_number - second_number
+    return sum_of_numbers
+
+def multiply(first_number, second_number):
+    sum_of_numbers = first_number * second_number
+    return sum_of_numbers
+
+def divide(first_number, second_number):
+    sum_of_number = first_number / second_number
+    return sum_of_number
+
+def length_of_string(string):
+    return len(string)
+    
+def join_string(first_string, second_string):
+    sum_of_string = first_string + second_string
+    return sum_of_string
+
+def add_string_as_number(one, two):
+    first_string = int(one)
+    second_string = int(two)
+    sum_of_string = first_string + second_string 
+    return sum_of_string
+
+def number_to_full_month_name(num):
+    months ={
+        1: "January",
+        2: "February",
+        3: "March",
+        4: "April",
+        5: "May",
+        6: "June",
+        7: "July",
+        8: "August",
+        9: "September",
+        10: "October",
+        11: "November",
+        12: "December"
+
+    }
+    return months[num]
+
+def number_to_short_month_name(num):
+    months ={
+        1: "Jan",
+        2: "Feb",
+        3: "Mar",
+        4: "Apr",
+        5: "May",
+        6: "Jun",
+        7: "Jul",
+        8: "Aug",
+        9: "Sep",
+        10: "Oct",
+        11: "Nov",
+        12: "Dec"
+
+    }
+    return months[num]
+
+
+
+
+
